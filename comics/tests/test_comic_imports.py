c99ef97e8d9813917eae884cab44b9fe1dc8a90f
Brian Pepple <bdpepple@gmail.com>
2017-11-23 16:57:25 -0500

    Minor formatting fix for pep8 compliance.

--- comics/tests/test_comic_imports.py
+++ comics/tests/test_comic_imports.py
@@ -4,9 +4,9 @@ from ..utils.comicimporter import ComicImporter
 
 
 class ImporterTest(TestCase):
+
     def test_series_sort_name(self):
         res = 'Demon, The'
         ci = ComicImporter()
         sort_name = ci.create_series_sortname('The Demon')
         self.assertEqual(res, sort_name)
-        
